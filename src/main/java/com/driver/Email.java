6a862d3f62d4d0b1fee4367c467369759c3dc173
Jitender Kumar <jitenderkumar72658@gmail.com>
2023-02-18 23:33:10 +0530

    Submitting Email Assignment

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -4,6 +4,10 @@ public class Email {
 
     private String emailId;
     private String password;
+    private boolean containsUppercase;
+    private boolean containsLowercase;
+    private boolean containsDigit;
+    private boolean containsSpecialChar;
 
     public Email(String emailId){
         this.emailId = emailId;
@@ -25,5 +29,26 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(password.equals(oldPassword)) {
+             if(newPassword.length()<8) return;
+             for(int i =0;i<newPassword.length();i++){
+                 int val = newPassword.charAt(i);
+                 if(val>=65 && val<91){
+                     containsUppercase = true;
+                 }
+                 else if(val>=48 && val<58){
+                     containsDigit = true;
+                 }
+                 else if(val>=97 && val<123){
+                     containsLowercase = true;
+                 }
+                 else{
+                     containsSpecialChar = true;
+                 }
+             }
+             if(containsSpecialChar && containsLowercase && containsUppercase && containsDigit){
+                 this.password = newPassword;
+             }
+        }
     }
 }
--- src/main/java/com/driver/Gmail.java
+++ src/main/java/com/driver/Gmail.java
@@ -1,15 +1,26 @@
 package com.driver;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
+import java.text.SimpleDateFormat;
 
 public class Gmail extends Email {
 
     int inboxCapacity; //maximum number of mails inbox can store
     //Inbox: Stores mails. Each mail has date (Date), sender (String), message (String). It is guaranteed that message is distinct for all mails.
     //Trash: Stores mails. Each mail has date (Date), sender (String), message (String)
-    public Gmail(String emailId, int inboxCapacity) {
+    private HashMap<String,Message > hminbox;
+    //private HashMap<String,Message > hmtrash;
+    private Deque<String> inbox;
+    private Stack<String> trash;
+    private Stack <String> temp;
 
+    public Gmail(String emailId, int inboxCapacity) {
+              super(emailId);
+             this.inboxCapacity = inboxCapacity;
+            hminbox = new HashMap<>();
+            inbox = new LinkedList<String>();
+            trash = new Stack<>();
+           // hmtrash = new HashMap<>();
     }
 
     public void receiveMail(Date date, String sender, String message){
@@ -17,49 +28,85 @@ public class Gmail extends Email {
         // It is guaranteed that:
         // 1. Each mail in the inbox is distinct.
         // 2. The mails are received in non-decreasing order. This means that the date of a new mail is greater than equal to the dates of mails received already.
-
+            hminbox.put(message, new Message(date,sender));
+            inbox.addFirst(message);
     }
 
     public void deleteMail(String message){
         // Each message is distinct
         // If the given message is found in any mail in the inbox, move the mail to trash, else do nothing
-
+         if(hminbox.containsKey(message)){
+             temp = new Stack<>();
+             while(inbox.size()>0 && inbox.peek().equals(message)){
+                 temp.push(inbox.removeFirst());
+             }
+             String copy = inbox.removeLast();
+            // hmtrash.put(copy,hminbox.get(copy));
+             hminbox.remove(copy);
+             trash.push(copy);
+             while(temp.size()>0){
+                 inbox.add(temp.pop());
+             }
+         }
     }
 
     public String findLatestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the latest mail present in the inbox
-
+          if(inbox.size()==0) return null;
+          else return inbox.peekFirst();
     }
 
     public String findOldestMessage(){
         // If the inbox is empty, return null
         // Else, return the message of the oldest mail present in the inbox
-
+        if(inbox.size()==0) return null;
+        else return inbox.peekLast();
     }
 
     public int findMailsBetweenDates(Date start, Date end){
         //find number of mails in the inbox which are received between given dates
         //It is guaranteed that start date <= end date
-
+        int count = 0;
+        for (Iterator itr = inbox.iterator();
+             itr.hasNext();) {
+           String s = (String)itr.next();
+           Message m = hminbox.get(s);
+           if(m.date.compareTo(start)>=0 && m.date.compareTo(end)<=0){
+               count++;
+           }
+        }
+          return count;
     }
 
     public int getInboxSize(){
         // Return number of mails in inbox
-
+              return inbox.size();
     }
 
     public int getTrashSize(){
         // Return number of mails in Trash
-
+            return trash.size();
     }
 
     public void emptyTrash(){
         // clear all mails in the trash
-
+         while(trash.size()>0) {
+             trash.pop();
+         }
     }
 
     public int getInboxCapacity() {
         // Return the maximum number of mails that can be stored in the inbox
+        return this.inboxCapacity;
+    }
+    class Message{
+        String sender;
+        Date date;
+        Message(Date date,String sender ){
+            this.date = date;
+            this.sender = sender;
+        }
     }
 }
+
